d32dbf2605c47be20c481c6e447a54bfb23b3395
davidcnitu <dcn.programming@gmail.com>
2022-11-03 22:57:22 +0000

    feat: Get meme images from imgflip api

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import dataset from '../memesData';
 
 export default function Meme() {
 
@@ -9,10 +8,19 @@ export default function Meme() {
         randomImage: "http://i.imgflip.com/1bij.jpg"
     });
 
-    const [allMemeImages, setAllMemeImages] = React.useState(dataset.data.memes);
+    const [allMemeImages, setAllMemeImages] = React.useState([]);
+
+    React.useEffect(() => {
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemeImages(data.data.memes)
+        }
+        getMemes()
+    }, [])
 
     function getMemeImage() {
-        const randomNumber = Math.floor((Math.random() * dataset.data.memes.length));
+        const randomNumber = Math.floor((Math.random() * allMemeImages.length));
         const { url } = allMemeImages[randomNumber];
         setMeme(prevMeme => ({ ...prevMeme, randomImage: url }));
     }
